f8be2bb26191139527e30d7dfd248ea764286723
yovorichard3-lgtm <yovorichard3@gmail.com>
2026-03-03 12:18:37 +0100

    java script

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("cc")
+function applyTheme(theme) {
+    document.documentElement.classList.toggle("theme-light", theme === "light")
+    const btnTheme = document.querySelector("#btn-theme")
+    btnTheme.textContent = theme === "light" ? "obscure" : "claire"
+}
+
+document.querySelector("#btn-theme").addEventListener("click", () => {
+    const isLight = document.documentElement.classList.contains("theme-light")
+    const next = isLight ? "dark" : "light"
+    applyTheme(next)
+})
