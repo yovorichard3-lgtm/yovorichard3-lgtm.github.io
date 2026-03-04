500eb8403b10635267cf1e0454a2652f5adf8c77
yovorichard3-lgtm <yovorichard3@gmail.com>
2026-03-04 15:10:58 +0100

    langue

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const translations ={
+    fr:{
+        tittle: "je ne sais pas",
+    },
+    en:{
+        tittle: "i don't know",
+    },
+
+}
+
+
+
+
+
+
 //fonction = truc qui est en bas (dessous)
 function applyTheme(theme) {
     document.documentElement.classList.toggle("theme-light", theme === "light")
@@ -5,7 +20,21 @@ function applyTheme(theme) {
     iconTheme.setAttribute("data-lucide",theme === "light" ? "moon" : "sun" ) 
     lucide.createIcons(); 
 }
+ function applyLang(lang) {
+    document.documentElement.lang = lang
+    const dict =translations[lang]
+    document.querySelectorAll("[data-i18n]").forEach((el)=>{
+        const key=el.getAttribute("data-i18n")
+        const value= dict[key]
+        if(value) el.textContent= value
+    })
+    const btnlang= document.querySelector("#btn-lang")
+    btnlang.textContent=lang
+ }
  
+
+
+
 
 
 
@@ -21,5 +50,10 @@ function applyTheme(theme) {
         localStorage.setItem("theme", next);
         applyTheme(next)
     })
-
+  document.querySelector("#btn-lang").addEventListener("click", () => {
+        const curent = document.documentElement.lang || "fr"
+        const next = curent === "fr"? "en": "fr"
+        localStorage.setItem("lang", next);
+        applyLang(next)
+    })
 }) ()
